772d36cd52d5662237fd4230057f153d2697905a
beatrizmakowski <bemakow@gmail.com>
2022-04-29 18:52:15 -0300

    feat(math-regex): add exemplos.js

---
+++ javascript-intermediario/match-regex/exemplos.js
@@ -0,0 +1,32 @@
+/*
+ * Luiza Code 4ª edição
+ * JavaScript Intermediário > Match && Regex
+ * Data: 29/04/2022
+ * 
+ */
+
+// As regular expressions (regex) são padrões comumente utilizados para selecionar, testar, ou
+// validar um conjunto de caracteres. Sua utilização é bem comum principamente na validação de
+// dados em uma string.
+// Por exemplo, ao receber os dados de uma pessoa em um formulário, é possível checar se o dado
+// inserido é válido e possui a formatação esperada.
+//
+// Exemplo de validação de placa de carro:
+let placa = 'AAA-0000'
+let reg = /^[A-Z]{3}\-\d{4}$/g
+placa.match(reg)
+
+// CPF
+// ^\d{3}\.\d{3}\.\d{3}\-\d{2}$
+
+// Data
+// ^\d{2}\/\d{2}\/\d{4}$
+
+// Começa e termina com uma "/"
+// Possui um conjunto de instruções e flags
+// g -> global
+// m -> multiline
+// i -> case insensitive
+
+placa = 'FJq-5837'
+console.log(placa.match(/^[A-Z]{3}\-\d{4}$/gm));
